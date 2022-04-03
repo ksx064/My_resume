a04fc42980b364d95fb11e4ede12be1bb3c4fd0f
KenWang <ken0711.wang@gmail.com>
2022-04-03 14:12:54 +0800

    first

---
+++ js.js
@@ -0,0 +1,9 @@
+const $menu = $('#navbarToggleExternalContent');
+
+$menu.on('show.bs.collapse', function () {
+  $menu.addClass('menu-show');
+});
+
+$menu.on('hide.bs.collapse', function () {
+  $menu.removeClass('menu-show');
+});
